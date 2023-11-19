12cd1187d700795e7a3d32d76038dbaeeb23a839
attal02 <ayaattal02@gmail.com>
2023-11-18 22:06:14 -0800

    added comics carousel
    still needs arrows

--- project 2/aya-p2/script.js
+++ project 2/aya-p2/script.js
@@ -1,8 +1,43 @@
 $(document).ready(function(){
   $('.hero-home').slick({
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 3000,
     dots: true,
   });
 });
-		
+
+$('.comics-carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+});
+
+"use strict"
+
+function toggleIcon() {
+  $(".icon").on("click", function () {
+    $(".icon").toggleClass("active")
+    $(".menu-mobile").slideToggle(300)
+  })
+}
+
+function closeMobileMenu() {
+  $(".menu-mobile").on("click", "a", function () {
+    $(".icon").trigger("click")
+  })
+}
+
+
+function desktopMenu() {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+      $("nav.desktop").fadeIn(500)
+    }
+  })
+}
+
+//when the page loads call toggleIcon;
+$(toggleIcon)
+$(closeMobileMenu)
+$(desktopMenu)
